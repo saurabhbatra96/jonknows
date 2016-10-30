77d1284c6b857785e345470a31e5affc5781f637
Saurabh Batra <saurabh.batra96@gmail.com>
2016-10-30 09:35:55 +0530

    almost done biatch

--- app/home/home.js
+++ app/home/home.js
@@ -29,6 +29,10 @@ state = "empty";
             if (resultvalue.indexOf(" ")>=0) {
               state = "sentence";
               getKeywords(resultvalue);
+              if ((resultvalue.substring(resultvalue.indexOf(".")+1)).indexOf(".")>=0)
+                parasenti(resultvalue);
+              else
+                sentencesenti(resultvalue);
             } else if (resultvalue=="") {
               state = "empty";
               cleanScreen();
@@ -82,7 +86,6 @@ state = "empty";
     xhrObj.onreadystatechange = function() {
         // console.log('readyState '+xhrObj.readyState);
         if (xhrObj.readyState == 4) {
-          console.log(xhrObj);
           var synonymdata = JSON.parse(xhrObj.responseText);
           synonymdata = synonymdata.response;
           var isnoun = false;
@@ -120,6 +123,156 @@ state = "empty";
 
     xhrObj.open("GET", url, true);
     xhrObj.send(null);
+  }
+
+  function parasenti(paragraph) {
+
+  	var sentences = paragraph.split(".");
+  	var sentis = new Array();
+    var i;
+
+  	for(i=0;i<sentences.length;i++) {
+  		if(sentences[i].length == 0) {
+  			sentences.splice(i,1);
+  		}
+  	}
+
+  	for(i = 0; i<sentences.length; i++) { // sentiment analysis of each sentence of paragraph
+  		var xhrObj = new XMLHttpRequest();
+  		var url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+  		var data = '{ "documents": [ { "language":"en", "id":"1", "text":"' + sentences[i] + '" } ] }';
+  		xhrObj.open("POST", url, false);
+  		xhrObj.setRequestHeader("Content-Type","application/json");
+  		xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","7514572919c74af9a97d235f9a9355d4");
+  		xhrObj.send(data);
+
+  		var JSONobj = JSON.parse(xhrObj.responseText);
+  		sentis.push(JSONobj.documents[0].score);
+  	}
+
+  	// console.log(sentis);
+
+  	var min = sentis[0]; var min_pos = 0;
+  	for(i=0;i<sentis.length;i++) {
+  		if(sentis[i] < min) {
+  			min = sentis[i];
+  			min_pos = i;
+  		}
+  	}
+
+    sentences[min_pos] = sentences[min_pos].trim();
+    document.getElementById('third-heading').innerHTML = "Sentiment Analysis";
+    document.getElementById('parasenti-quote').innerHTML = "\"" + sentences[min_pos] + ".\"";
+    document.getElementById('parasenti-darkside').innerHTML = "Looks like the dark side is strong in this line. Select it for better suggestions."
+  }
+
+  function sentencesenti(sentence) {
+  	var words = sentence.split(" ");
+  	var i,j;
+  	var sentis = new Array();
+
+  	for(i = 0; i<words.length; i++) { // sentiment analysis of each word of sentence
+  		var xhrObj = new XMLHttpRequest();
+  		var url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+  		var data = '{ "documents": [ { "language":"en", "id":"1", "text":"' + words[i] + '" } ] }';
+  		xhrObj.open("POST", url, false);
+  		xhrObj.setRequestHeader("Content-Type","application/json");
+  		xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","7514572919c74af9a97d235f9a9355d4");
+  		xhrObj.send(data);
+
+  		var JSONobj = JSON.parse(xhrObj.responseText);
+  		sentis.push(JSONobj.documents[0].score);
+  	}
+
+  	// console.log(sentis);
+
+  	// getting synonyms of lowest sentiment word
+
+  	var min = 1.0, min_pos = 0;
+  	for(i=0; i<sentis.length; i++) {
+  		if(sentis[i]<min) {
+  			min = sentis[i];
+  			min_pos = i;
+  		}
+  	}
+
+  	var xhrObj = new XMLHttpRequest();
+  	var url = "http://thesaurus.altervista.org/thesaurus/v1?word=" + words[min_pos] + "&language=en_US&output=json&key=X6cKt6oU9RJjmYsBQqRw";
+  	xhrObj.open("GET", url, false);
+  	xhrObj.send(null);
+
+  	var syns = JSON.parse(xhrObj.responseText).response;
+
+  	var synonyms = [];
+
+  	for(i=0; i<syns.length; i++) {
+  		var s = (syns[i].list.synonyms).split("|");
+  		// console.log(s);
+  		for(j=0;j<s.length;j++) {
+  			synonyms.push(s[j]);
+  		}
+  	}
+
+  	for(i=0;i<synonyms.length;i++) {
+  		if(synonyms[i].split(" ").length > 1) {
+  			synonyms.splice(i,1);
+  		}
+  	}
+
+  	// console.log(synonyms);
+
+  	// replacing the word with all synonyms got and calling the joint dist API
+
+  	var lang_queries = [];
+  	for(i=0;i<synonyms.length;i++) {
+  		var str = "";
+  		for(j=0;j<words.length;j++) {
+  			if(j == min_pos)
+  				str = str+synonyms[i]+" ";
+  			else
+  				str = str+words[j]+" ";
+  		}
+  		lang_queries.push(str);
+  	}
+
+  	xhrObj = new XMLHttpRequest();
+  	var url = "https://api.projectoxford.ai/text/weblm/v1.0/calculateJointProbability?model=body";
+  	var data = '{ "queries": [';
+  	for(i=0;i<lang_queries.length;i++) {
+  		data = data + '"' + lang_queries[i]+ '", ';
+  	}
+  	data = data + '] }';
+  	xhrObj.open("POST", url, false);
+  	xhrObj.setRequestHeader("Content-Type","application/json");
+  	xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","96d1620188ce4d4c8d60074af57689c9");
+  	xhrObj.send(data);
+
+    // console.log(xhrObj.responseText);
+    var xmlDoc = jQuery.parseXML(xhrObj.responseText);
+    console.log(xmlDoc);
+    var results = xmlDoc.getElementsByTagName("JointProbabilityResult");
+    var suggestions = new Array();
+    var noofsugg = 2;
+
+    for (j=0;j<noofsugg;++j) {
+      min = results[0].childNodes[0].childNodes[0].nodeValue;
+      min_pos = 0;
+      for (i=0;i<results.length;++i) {
+        if (results[i].childNodes[0].childNodes[0].nodeValue < min) {
+          min = results[i].childNodes[0].childNodes[0].nodeValue;
+          min_pos = i;
+        }
+      }
+      suggestions.push(results[min_pos].childNodes[1].childNodes[0].nodeValue);
+      results[min_pos].childNodes[0].childNodes[0].nodeValue = 9999;
+    }
+
+    console.log(suggestions);
+    document.getElementById('third-heading').innerHTML = "Sentiment Analysis";
+    document.getElementById('sentsenti-1').innerHTML = "\"" + suggestions[0] + "\"";
+    document.getElementById('sentsenti-2').innerHTML = "\"" + suggestions[1] + "\"";
+    document.getElementById('sentsenti-sugg').innerHTML = "You might want to change a couple of words in there.";
+    document.getElementById('sentor').innerHTML = "OR";
   }
 
   function bingsearch(phrase){
@@ -198,5 +351,18 @@ state = "empty";
     document.getElementById('img-2').src = "";
     document.getElementById('img-3').src = "";
     document.getElementById('img-4').src = "";
+
+    //parasenti
+    document.getElementById('parasenti-quote').innerHTML = "";
+    document.getElementById('parasenti-darkside').innerHTML = "";
+    document.getElementById('third-heading').innerHTML = "";
+
+    //sentsenti
+    document.getElementById('third-heading').innerHTML = "";
+    document.getElementById('sentsenti-1').innerHTML = "";
+    document.getElementById('sentsenti-2').innerHTML = "";
+    document.getElementById('sentsenti-sugg').innerHTML = "";
+    document.getElementById('sentor').innerHTML = "";
   }
+
 })();
